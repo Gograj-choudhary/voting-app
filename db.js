e5364b86dd106de37606c8c6c4729a311e51dac9
GOGRAJ DADARWAL <ramkunwarjat00@gmail.com>
2025-02-02 22:43:15 +0530

    data base switch on mongoDb Atlas

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // MongoDB connection LOCAL URL
-const mongoURL = process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
 
 // mongoDB Atlas URL
 //const mongoURL = process.env.MONGODB_URL;
