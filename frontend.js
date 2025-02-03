7f61842e84e7ab145729cd4e4ed1ef7b28a54dc4
GOGRAJ DADARWAL <ramkunwarjat00@gmail.com>
2025-02-03 23:15:31 +0530

    Update frontend.js

--- frontend.js
+++ frontend.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
-
 const app = express();
 
 const getFilePath = (relativeFilePath) => {
